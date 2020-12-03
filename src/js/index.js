250cb24a7680f47e3fda995fa40acb6e5d2fd4c0
sathyaramkumar <70505050+sathyaramkumar@users.noreply.github.com>
2020-12-03 14:52:26 +0530

    adding hamburger menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,36 @@
+function onClick() {
+    var hamburger = document.getElementById('navbarNav');
+    if (hamburger.style.display === "block") {
+        hamburger.style.display = "none";
+    }
+    else {
+        hamburger.style.display = "block";
+    }
+}
+
+
 
 function nightMood() {
     var x = document.getElementById('nightMood');
     var y = document.getElementById('bottonOn');
 
-    if (x.style.backgroundColor === "white" || x.style.color === "back") {
-        x.style.backgroundColor = "black";
-        x.style.color = "white";
-        y.innerHTML = "Night mood";
-        y.style.backgroundColor = "white";
-        y.style.color = "black";
-    } else {
+    if (x.style.backgroundColor === "black" || x.style.color === "white") {
+
         x.style.backgroundColor = "white";
-        x.style.color = "black"
+        x.style.color = "black";
+
         y.innerHTML = "Day mood";
         y.style.backgroundColor = "black";
         y.style.color = "white";
+
+    } else {
+        x.style.backgroundColor = "black";
+        x.style.color = "white";
+
+        y.innerHTML = "Night mood";
+        y.style.backgroundColor = "white";
+        y.style.color = "black";
     }
 }
+
 
